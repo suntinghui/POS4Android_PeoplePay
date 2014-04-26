3ad83d9ea5bd468aae6b48e31827c1c5349d1e6a
liaojia <liaojia19880128@163.com>
2014-04-26 15:33:38 +0800

    lj

--- gen/com/people/R.java
+++ gen/com/people/R.java
@@ -63,6 +63,7 @@ public final class R {
         public static final int spinner_7=0x7f02001a;
         public static final int spinner_8=0x7f02001b;
         public static final int spinner_9=0x7f02001c;
+        public static final int splash=0x7f02001d;
     }
     public static final class id {
         public static final int action_settings=0x7f090009;
@@ -78,10 +79,11 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
-        public static final int alert_dialog_layout=0x7f030001;
-        public static final int progress_dialog_layout=0x7f030002;
-        public static final int progress_hud=0x7f030003;
-        public static final int schedule_dialog_layout=0x7f030004;
+        public static final int activity_splash=0x7f030001;
+        public static final int alert_dialog_layout=0x7f030002;
+        public static final int progress_dialog_layout=0x7f030003;
+        public static final int progress_hud=0x7f030004;
+        public static final int schedule_dialog_layout=0x7f030005;
     }
     public static final class menu {
         public static final int login=0x7f080000;
@@ -91,6 +93,7 @@ public final class R {
         public static final int app_name=0x7f060000;
         public static final int cancel=0x7f060005;
         public static final int hello_world=0x7f060002;
+        public static final int noNetTips=0x7f060008;
         public static final int ok=0x7f060004;
         public static final int title_alert=0x7f060003;
         public static final int title_progressbar=0x7f060007;
