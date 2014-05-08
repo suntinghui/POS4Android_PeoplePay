1dafe570e16bd40949137a90461c5555baf5afed
liaojia <liaojia19880128@163.com>
2014-05-08 16:36:47 +0800

    lj

--- gen/com/people/R.java
+++ gen/com/people/R.java
@@ -23,11 +23,26 @@ public final class R {
     }
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f050002;
+        public static final int blue=0x7f050004;
+        public static final int blue_0=0x7f050008;
+        public static final int clear=0x7f05000b;
+        public static final int gray=0x7f050006;
+        public static final int green=0x7f050007;
+        public static final int instructions=0x7f05000c;
+        public static final int lightblack=0x7f050009;
+        public static final int orange=0x7f050005;
+        public static final int red=0x7f050003;
+        public static final int splash_bg=0x7f050000;
+        public static final int transparent_background=0x7f05000a;
+        public static final int white=0x7f050001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int app_list_corner_round=0x7f020000;
@@ -43,107 +58,157 @@ public final class R {
         public static final int btn_ok_selector=0x7f02000a;
         public static final int dialog_bottom_bg=0x7f02000b;
         public static final int dis_behind_side=0x7f02000c;
-        public static final int ic_launcher=0x7f02000d;
-        public static final int icon_n_0=0x7f02000e;
-        public static final int icon_n_1=0x7f02000f;
-        public static final int icon_n_2=0x7f020010;
-        public static final int icon_n_3=0x7f020011;
-        public static final int icon_s_0=0x7f020012;
-        public static final int icon_s_1=0x7f020013;
-        public static final int icon_s_2=0x7f020014;
-        public static final int icon_s_3=0x7f020015;
-        public static final int nodata=0x7f020016;
-        public static final int progress_hud_bg=0x7f020017;
-        public static final int progressbar_circle=0x7f020018;
-        public static final int progressbar_horizontal=0x7f020019;
-        public static final int spinner_0=0x7f02001a;
-        public static final int spinner_1=0x7f02001b;
-        public static final int spinner_10=0x7f02001c;
-        public static final int spinner_11=0x7f02001d;
-        public static final int spinner_2=0x7f02001e;
-        public static final int spinner_3=0x7f02001f;
-        public static final int spinner_4=0x7f020020;
-        public static final int spinner_5=0x7f020021;
-        public static final int spinner_6=0x7f020022;
-        public static final int spinner_7=0x7f020023;
-        public static final int spinner_8=0x7f020024;
-        public static final int spinner_9=0x7f020025;
-        public static final int splash=0x7f020026;
-        public static final int text=0x7f020027;
-        public static final int touming_bg=0x7f020028;
+        public static final int head_image=0x7f02000d;
+        public static final int ic_launcher=0x7f02000e;
+        public static final int icon_n_0=0x7f02000f;
+        public static final int icon_n_1=0x7f020010;
+        public static final int icon_n_2=0x7f020011;
+        public static final int icon_n_3=0x7f020012;
+        public static final int icon_s_0=0x7f020013;
+        public static final int icon_s_1=0x7f020014;
+        public static final int icon_s_2=0x7f020015;
+        public static final int icon_s_3=0x7f020016;
+        public static final int nodata=0x7f020017;
+        public static final int progress_hud_bg=0x7f020018;
+        public static final int progressbar_circle=0x7f020019;
+        public static final int progressbar_horizontal=0x7f02001a;
+        public static final int spinner_0=0x7f02001b;
+        public static final int spinner_1=0x7f02001c;
+        public static final int spinner_10=0x7f02001d;
+        public static final int spinner_11=0x7f02001e;
+        public static final int spinner_2=0x7f02001f;
+        public static final int spinner_3=0x7f020020;
+        public static final int spinner_4=0x7f020021;
+        public static final int spinner_5=0x7f020022;
+        public static final int spinner_6=0x7f020023;
+        public static final int spinner_7=0x7f020024;
+        public static final int spinner_8=0x7f020025;
+        public static final int spinner_9=0x7f020026;
+        public static final int splash=0x7f020027;
+        public static final int start_background=0x7f020028;
+        public static final int swip_icon_n_0=0x7f020029;
+        public static final int swip_icon_n_1=0x7f02002a;
+        public static final int swip_icon_n_2=0x7f02002b;
+        public static final int swip_icon_n_3=0x7f02002c;
+        public static final int swip_icon_n_4=0x7f02002d;
+        public static final int swip_icon_n_5=0x7f02002e;
+        public static final int swip_icon_n_6=0x7f02002f;
+        public static final int swip_icon_n_7=0x7f020030;
+        public static final int swip_icon_n_8=0x7f020031;
+        public static final int text=0x7f020032;
+        public static final int top=0x7f020033;
+        public static final int touming_bg=0x7f020034;
     }
     public static final class id {
-        public static final int bottomLayout=0x7f080020;
-        public static final int btn1=0x7f08000a;
-        public static final int btn3=0x7f08000f;
-        public static final int btnGetVes=0x7f08000b;
-        public static final int btnMag=0x7f08000c;
-        public static final int btnMagPwd=0x7f08000e;
-        public static final int btnPowerOff=0x7f080008;
-        public static final int btnPwd=0x7f08000d;
-        public static final int btnUpgradeKey=0x7f080009;
-        public static final int btn_login=0x7f080002;
-        public static final int btn_register=0x7f080001;
-        public static final int choosedevice=0x7f080007;
-        public static final int closedevice=0x7f080006;
-        public static final int detail=0x7f080024;
-        public static final int device_address=0x7f08001c;
-        public static final int device_bonded=0x7f08001d;
-        public static final int device_btn=0x7f08001a;
-        public static final int device_list=0x7f080019;
-        public static final int device_name=0x7f08001b;
-        public static final int device_progress=0x7f080017;
-        public static final int device_rssi=0x7f08001e;
-        public static final int device_txt=0x7f080018;
-        public static final int deviceaddress=0x7f080004;
-        public static final int dialog_data_load=0x7f080011;
-        public static final int dialog_data_load_txt=0x7f080012;
-        public static final int layout1=0x7f080000;
-        public static final int message=0x7f080014;
-        public static final int negativeButton=0x7f080016;
-        public static final int opendevice=0x7f080005;
-        public static final int positiveButton=0x7f080015;
-        public static final int progress=0x7f080023;
-        public static final int progressBar=0x7f080022;
-        public static final int spinnerImageView=0x7f080021;
-        public static final int status=0x7f080003;
-        public static final int tab_iv_icon=0x7f08001f;
-        public static final int text1=0x7f080010;
-        public static final int title=0x7f080013;
+        public static final int bottomLayout=0x7f09003b;
+        public static final int btn1=0x7f090024;
+        public static final int btn3=0x7f090029;
+        public static final int btnGetVes=0x7f090025;
+        public static final int btnMag=0x7f090026;
+        public static final int btnMagPwd=0x7f090028;
+        public static final int btnPowerOff=0x7f090022;
+        public static final int btnPwd=0x7f090027;
+        public static final int btnUpgradeKey=0x7f090023;
+        public static final int btn_0=0x7f090012;
+        public static final int btn_1=0x7f090005;
+        public static final int btn_2=0x7f090006;
+        public static final int btn_3=0x7f090007;
+        public static final int btn_4=0x7f090009;
+        public static final int btn_5=0x7f09000a;
+        public static final int btn_6=0x7f09000b;
+        public static final int btn_7=0x7f09000d;
+        public static final int btn_8=0x7f09000e;
+        public static final int btn_9=0x7f09000f;
+        public static final int btn_add=0x7f090008;
+        public static final int btn_back=0x7f090003;
+        public static final int btn_c=0x7f090011;
+        public static final int btn_div=0x7f090014;
+        public static final int btn_eq=0x7f090013;
+        public static final int btn_exit=0x7f09001b;
+        public static final int btn_login=0x7f090016;
+        public static final int btn_mul=0x7f090010;
+        public static final int btn_register=0x7f090001;
+        public static final int btn_sub=0x7f09000c;
+        public static final int catalogCellImage=0x7f090039;
+        public static final int choosedevice=0x7f090021;
+        public static final int closedevice=0x7f090020;
+        public static final int detail=0x7f09003f;
+        public static final int device_address=0x7f090036;
+        public static final int device_bonded=0x7f090037;
+        public static final int device_btn=0x7f090034;
+        public static final int device_list=0x7f090033;
+        public static final int device_name=0x7f090035;
+        public static final int device_progress=0x7f090031;
+        public static final int device_rssi=0x7f090038;
+        public static final int device_txt=0x7f090032;
+        public static final int deviceaddress=0x7f09001e;
+        public static final int dialog_data_load=0x7f09002b;
+        public static final int dialog_data_load_txt=0x7f09002c;
+        public static final int gridveiw=0x7f09001c;
+        public static final int gv=0x7f090015;
+        public static final int ibtn_head=0x7f090017;
+        public static final int layout1=0x7f090000;
+        public static final int layout_msg_blow=0x7f09001a;
+        public static final int layout_msg_top=0x7f090019;
+        public static final int message=0x7f09002e;
+        public static final int negativeButton=0x7f090030;
+        public static final int opendevice=0x7f09001f;
+        public static final int positiveButton=0x7f09002f;
+        public static final int progress=0x7f09003e;
+        public static final int progressBar=0x7f09003d;
+        public static final int spinnerImageView=0x7f09003c;
+        public static final int status=0x7f09001d;
+        public static final int tab_iv_icon=0x7f09003a;
+        public static final int text1=0x7f09002a;
+        public static final int title=0x7f09002d;
+        public static final int titleTV=0x7f090004;
+        public static final int topbarLayout=0x7f090002;
+        public static final int tv_head=0x7f090018;
     }
     public static final class layout {
         public static final int activity_catalog=0x7f030000;
         public static final int activity_flow=0x7f030001;
         public static final int activity_forget_login_pwd=0x7f030002;
-        public static final int activity_login=0x7f030003;
-        public static final int activity_merchant=0x7f030004;
-        public static final int activity_modify_login_pwd=0x7f030005;
-        public static final int activity_register=0x7f030006;
-        public static final int activity_splash=0x7f030007;
-        public static final int activity_swip=0x7f030008;
-        public static final int activity_test=0x7f030009;
-        public static final int activity_tool=0x7f03000a;
-        public static final int alert_dialog_layout=0x7f03000b;
-        public static final int choose_device=0x7f03000c;
-        public static final int device_item=0x7f03000d;
-        public static final int layout_tab=0x7f03000e;
-        public static final int progress_dialog_layout=0x7f03000f;
-        public static final int progress_hud=0x7f030010;
-        public static final int schedule_dialog_layout=0x7f030011;
+        public static final int activity_inputmoney=0x7f030003;
+        public static final int activity_lock_screen=0x7f030004;
+        public static final int activity_login=0x7f030005;
+        public static final int activity_merchant=0x7f030006;
+        public static final int activity_modify_login_pwd=0x7f030007;
+        public static final int activity_register=0x7f030008;
+        public static final int activity_splash=0x7f030009;
+        public static final int activity_swip=0x7f03000a;
+        public static final int activity_test=0x7f03000b;
+        public static final int activity_tool=0x7f03000c;
+        public static final int alert_dialog_layout=0x7f03000d;
+        public static final int choose_device=0x7f03000e;
+        public static final int device_item=0x7f03000f;
+        public static final int item_catalog=0x7f030010;
+        public static final int layout_tab=0x7f030011;
+        public static final int progress_dialog_layout=0x7f030012;
+        public static final int progress_hud=0x7f030013;
+        public static final int schedule_dialog_layout=0x7f030014;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int cancel=0x7f060005;
-        public static final int hello_world=0x7f060002;
-        public static final int noNetTips=0x7f060008;
-        public static final int ok=0x7f060004;
-        public static final int title_alert=0x7f060003;
-        public static final int title_progressbar=0x7f060007;
-        public static final int title_waitting=0x7f060006;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int bank_no=0x7f070009;
+        public static final int cancel=0x7f070005;
+        public static final int conect=0x7f07000f;
+        public static final int exit_safity=0x7f070010;
+        public static final int hello_world=0x7f070002;
+        public static final int in_account_msg=0x7f07000c;
+        public static final int modify_pwd=0x7f07000d;
+        public static final int more_setting=0x7f07000e;
+        public static final int noNetTips=0x7f070008;
+        public static final int ok=0x7f070004;
+        public static final int open_account_bank=0x7f07000b;
+        public static final int open_account_name=0x7f07000a;
+        public static final int title_alert=0x7f070003;
+        public static final int title_progressbar=0x7f070007;
+        public static final int title_waitting=0x7f070006;
     }
     public static final class style {
-        public static final int Animation=0x7f07000b;
+        public static final int Animation=0x7f08000b;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -154,23 +219,41 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
-        public static final int ContentOverlay=0x7f07000d;
-        public static final int Dialog=0x7f070008;
-        public static final int ProgressHUD=0x7f07000c;
+        public static final int AppTheme=0x7f080001;
+        public static final int ContentOverlay=0x7f08000d;
+        public static final int Dialog=0x7f080008;
+        public static final int ProgressHUD=0x7f08000c;
         /**  取消动画 
          */
-        public static final int Theme=0x7f07000a;
-        public static final int progressbarStyle_Horizontal=0x7f070009;
-        public static final int sdw_79351b=0x7f070005;
-        public static final int sdw_white=0x7f070004;
-        public static final int text_15_666666_sdw=0x7f070007;
-        public static final int text_15_ffffff_sdw=0x7f070006;
-        public static final int text_16_666666=0x7f070003;
-        public static final int text_18_ffffff=0x7f070002;
+        public static final int Theme=0x7f08000a;
+        public static final int black_14=0x7f08000e;
+        public static final int black_15=0x7f080014;
+        public static final int black_16=0x7f080017;
+        public static final int black_17=0x7f08001a;
+        public static final int black_18=0x7f08001f;
+        public static final int gray_14=0x7f080011;
+        public static final int gray_15=0x7f080012;
+        public static final int gree_14=0x7f080013;
+        public static final int gree_15=0x7f080016;
+        public static final int gree_17=0x7f080019;
+        public static final int green_17=0x7f08001b;
+        public static final int orange_14=0x7f080010;
+        public static final int orange_17=0x7f08001c;
+        public static final int progressbarStyle_Horizontal=0x7f080009;
+        public static final int sdw_79351b=0x7f080005;
+        public static final int sdw_white=0x7f080004;
+        public static final int text_15_666666_sdw=0x7f080007;
+        public static final int text_15_ffffff_sdw=0x7f080006;
+        public static final int text_16_666666=0x7f080003;
+        public static final int text_18_ffffff=0x7f080002;
+        public static final int white_14=0x7f08000f;
+        public static final int white_15=0x7f080015;
+        public static final int white_17=0x7f080018;
+        public static final int white_18=0x7f08001d;
+        public static final int white_20=0x7f08001e;
     }
 }
