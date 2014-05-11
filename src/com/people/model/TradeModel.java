07342e5d02953cd8f3e3549847f4ff451f14c592
suntinghui <tinghuisun@163.com>
2014-05-11 11:05:31 +0800

    STH

---
+++ src/com/people/model/TradeModel.java
@@ -0,0 +1,78 @@
+package com.people.model;
+
+public class TradeModel {
+
+	private String sysDate;
+	private String merName;
+	private String logDate;
+	private String logNo;
+	private String txncd;
+	private String txnsts;
+	private String amount;
+	private String cardNo;
+
+	public String getSysDate() {
+		return sysDate;
+	}
+
+	public void setSysDate(String sysDate) {
+		this.sysDate = sysDate;
+	}
+
+	public String getMerName() {
+		return merName;
+	}
+
+	public void setMerName(String merName) {
+		this.merName = merName;
+	}
+
+	public String getLogDate() {
+		return logDate;
+	}
+
+	public void setLogDate(String logDate) {
+		this.logDate = logDate;
+	}
+
+	public String getLogNo() {
+		return logNo;
+	}
+
+	public void setLogNo(String logNo) {
+		this.logNo = logNo;
+	}
+
+	public String getTxncd() {
+		return txncd;
+	}
+
+	public void setTxncd(String txncd) {
+		this.txncd = txncd;
+	}
+
+	public String getTxnsts() {
+		return txnsts;
+	}
+
+	public void setTxnsts(String txnsts) {
+		this.txnsts = txnsts;
+	}
+
+	public String getAmount() {
+		return amount;
+	}
+
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+
+	public String getCardNo() {
+		return cardNo;
+	}
+
+	public void setCardNo(String cardNo) {
+		this.cardNo = cardNo;
+	}
+
+}
