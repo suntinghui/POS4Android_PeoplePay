d8e8ce690f144dce431d25c2ff0e779806f5a0ed
liaojia <liaojia19880128@163.com>
2014-05-14 16:39:15 +0800

    lj

--- src/com/people/activity/FeedBackActivity.java
+++ src/com/people/activity/FeedBackActivity.java
@@ -1,22 +1,11 @@
 package com.people.activity;
 
-import java.util.Date;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.people.R;
-import com.people.client.AppDataCenter;
-import com.people.client.ApplicationEnvironment;
-import com.people.client.Constants;
-import com.people.client.TransferRequestTag;
-import com.people.model.TradeModel;
-import com.people.util.DateUtil;
-import com.people.util.StringUtil;
 
 // 意见反馈
 public class FeedBackActivity extends BaseActivity implements OnClickListener {
