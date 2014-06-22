f0a7403214a0ba71e2b1e0071a2c6276b40f1952
suntinghui <tinghuisun@163.com>
2014-06-22 22:12:49 +0800

    STH

---
+++ src/com/people/activity/WithdrawalCashActivity.java
@@ -0,0 +1,18 @@
+package com.people.activity;
+
+import com.people.R;
+
+import android.os.Bundle;
+
+// 提现
+public class WithdrawalCashActivity extends BaseActivity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_withdrawal_cash);
+		
+	}
+
+}
